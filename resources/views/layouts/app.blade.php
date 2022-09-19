1929f4a6680f71be810401f80b6ae3a1bd13cfbf
fredericodgv <fredericodgv@gmail.com>
2022-09-18 23:38:03 -0300

    Alguns ajustes iniciais de Front-end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,16 +5,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    <script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
 
     <title>{{ config('app.name', 'Code-Play') }}</title>
 </head>
 
 <body>
-    @extends('layouts.navigation')
-    
-    <h1 class="text-3xl font-bold underline">
-        Hello World
-    </h1>
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <header class="bg-blue-200 shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ $title ?? 'Code-Play' }}
+                </h2>
+            </div>
+        </header>
+
+        <main>
+            
+        </main>
+    </div>
 </body>
 
 </html>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,16 @@
-<div class="sm:flex">
-    <x-nav-link :active="request()->routeIs('layouts.app')" href="{{ route('layouts.app') }}">
-        {{ __('Dashboard') }}
-    </x-nav-link>
-</div>
+<nav x-data="{ open: false }" class="bg-blue-300 border-b border-indigo-200">
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :active="request()->routeIs('players.index')" href="{{ route('players.index') }}">
+                        {{ __('Jogadores') }}
+                    </x-nav-link>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</nav>
